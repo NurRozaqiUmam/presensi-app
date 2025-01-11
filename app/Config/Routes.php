cd399b03c938bb926c85196a88811290f30fa9c2
Nur Rozaqi Umam <nurrozaqiumam16@gmail.com>
2025-01-11 13:45:07 +0700

    create fitur login and add filters in controller

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,9 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Login::index');
+$routes->post('login', 'Login::login_action');
+$routes->get('logout', 'Login::logout');
+
+$routes->get('admin/home', 'Admin\Home::index', ['filter' => 'adminFilter']);
+$routes->get('pegawai/home', 'Pegawai\Home::index', ['filter' => 'pegawaiFilter']);
